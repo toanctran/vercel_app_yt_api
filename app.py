3ff1ade349d124d8db535c28f7192ef5562f854c
TOAN TRAN <chitoantran@outlook.com>
2023-11-24 11:14:17 +0700

    update app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from fastapi.openapi.utils import get_openapi
 import json
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import JSONFormatter
-
+import requests
 
 app = FastAPI()
 
@@ -12,13 +12,30 @@ app = FastAPI()
 async def root():
   return{"message":"hello world"}
 
-@app.get("/get_transcript/{language}/{video_id}")
+@app.get("/api/get_transcript/{video_id}/{language}")
 async def get_transcript(request: Request, video_id, language):
-  transcript = YouTubeTranscriptApi.get_transcript(video_id, [f'{language}'])
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, [f'{language}'])
 
-  formatter = JSONFormatter()
+    formatter = JSONFormatter()
 
-  # .format_transcript(transcript) turns the transcript into a JSON string.
-  json_formatted = formatter.format_transcript(transcript)
-  return json_formatted
-    
+    # .format_transcript(transcript) turns the transcript into a JSON string.
+    json_formatted = formatter.format_transcript(transcript)
+    return json_formatted
+
+@app.get("/api/getYTVideoTitle/{video_id}")
+async def get_yt_video_title(request: Request, video_id: str):
+    try:
+        # Replace 'YOUR_YOUTUBE_API_KEY' with your actual YouTube Data API key
+        api_key = 'AIzaSyDV9XQkANrjxF9GF-30N6c0gm4GhuGZX3Y'
+
+        # Make an HTTP GET request to the YouTube Data API
+        response = requests.get(
+            f'https://www.googleapis.com/youtube/v3/videos?id={video_id}&key={api_key}&part=snippet'
+        )
+
+        # Extract the video title from the API response
+        video_title = response.json()['items'][0]['snippet']['title']
+
+        return {"title": video_title}
+    except Exception as e:
+        return {"error": str(e)}
